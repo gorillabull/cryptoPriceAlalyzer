fb1f4c9b3365ef01dff2a528e48476050b418747
gorillabull <thisisntlongonlytomakelifehard@gmail.com>
2020-02-07 20:28:01 -0600

    added sobel

--- CryptoPriceAnalyzer1/Window1.Designer.cs
+++ CryptoPriceAnalyzer1/Window1.Designer.cs
@@ -33,6 +33,9 @@
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
@@ -77,11 +80,51 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(193, 113);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Gauss";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_CheckedChanged);
+           
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(193, 136);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(52, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Sobel";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(193, 159);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Click += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
             // Window1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1185, 692);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.pictureBox1);
@@ -103,5 +146,8 @@
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
     }
 }
--- CryptoPriceAnalyzer1/Window1.cs
+++ CryptoPriceAnalyzer1/Window1.cs
@@ -68,6 +68,7 @@ namespace CryptoPriceAnalyzer1
                 file = openFileDialog1.OpenFile();
                 image = new Bitmap(file);
                 pictureBox1.Image = image;
+                trackBar1.Value = 0;
             }
             
         }
@@ -77,6 +78,55 @@ namespace CryptoPriceAnalyzer1
             int blurval = trackBar1.Value;
             DateTime start = DateTime.Now;
 
+            if (radioButton1.Checked==true)
+            {
+                image = gaussianFilter(image, blurval);
+            }
+            else
+            {
+                if (radioButton2.Checked==true)
+                {
+                    image = Sobel(image);
+                }
+                else
+                {
+                    if (radioButton3.Checked == true)
+                    {
+
+                    }
+                }
+            }
+           
+
+            pictureBox1.Image = image;
+            TimeSpan duration = DateTime.Now - start;
+
+
+            richTextBox1.Text = "Finished in " + duration.ToString() + " milliseconds." ;
+        }
+
+
+        private static double[] Create1DGaussianKernel(double sd)
+        {
+            // Normally software will compute the size of the kernel required to adequately capture the standard deviation of the gaussian. 2.5 x sd radius is plenty.
+            int radius = (int)Math.Ceiling(sd * 2.5);
+
+            double[] kernel = new double[radius * 2 + 1];
+            int kernelPosition = 0;
+
+            // You can see the formula I've used for the Gaussian function at http://en.wikipedia.org/wiki/Normal_distribution.
+            double norm = 1 / Math.Sqrt(2 * Math.PI * sd * sd);
+
+            for (int u = -radius; u <= radius; u++)
+            {
+                kernel[kernelPosition++] = norm * Math.Exp(-(u * u) / (2 * sd * sd));
+            }
+
+            return kernel;
+        }
+
+        private static unsafe Bitmap gaussianFilter(Bitmap image, int blurval)
+        {
             //change the pixel format. 
             //image = new Bitmap(image.Width, image.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
             image = image.Clone(new Rectangle(0, 0, image.Width, image.Height), PixelFormat.Format32bppArgb);
@@ -85,7 +135,7 @@ namespace CryptoPriceAnalyzer1
             // 32ARGB and 32RGB are the same, except in the latter the alpha byte is simply padding.
             if (image.PixelFormat != PixelFormat.Format32bppArgb && image.PixelFormat != PixelFormat.Format32bppRgb)
             {
-                return;
+                return null;
             }
 
             int width = image.Width;
@@ -199,32 +249,220 @@ namespace CryptoPriceAnalyzer1
 
             // Finish with image and save
             image.UnlockBits(imageData);
-
-            pictureBox1.Image = image;
-            TimeSpan duration = DateTime.Now - start;
-
-
-            richTextBox1.Text = "Finished in " + duration.ToString() + " milliseconds." ;
+            return image;
         }
 
+        private static unsafe Bitmap Sobel (Bitmap image){
 
-        private static double[] Create1DGaussianKernel(double sd)
-        {
-            // Normally software will compute the size of the kernel required to adequately capture the standard deviation of the gaussian. 2.5 x sd radius is plenty.
-            int radius = (int)Math.Ceiling(sd * 2.5);
+            //change the pixel format. 
+            //image = new Bitmap(image.Width, image.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            image = image.Clone(new Rectangle(0, 0, image.Width, image.Height), PixelFormat.Format32bppArgb);
 
-            double[] kernel = new double[radius * 2 + 1];
-            int kernelPosition = 0;
 
-            // You can see the formula I've used for the Gaussian function at http://en.wikipedia.org/wiki/Normal_distribution.
-            double norm = 1 / Math.Sqrt(2 * Math.PI * sd * sd);
-
-            for (int u = -radius; u <= radius; u++)
+            // This simple code is meant for 32bpp images, it could be adapted to handle other image types.
+            // 32ARGB and 32RGB are the same, except in the latter the alpha byte is simply padding. We ignore alpha in this code anyway.
+            if (image.PixelFormat != PixelFormat.Format32bppArgb
+                && image.PixelFormat != PixelFormat.Format32bppRgb)
             {
-                kernel[kernelPosition++] = norm * Math.Exp(-(u * u) / (2 * sd * sd));
+                return null ;
             }
 
-            return kernel;
+            // Obtain grayscale conversion of the image
+            byte[] grayData = ConvertTo8bpp(image);
+
+            int width = image.Width;
+            int height = image.Height;
+
+            // Buffers
+            byte[] buffer = new byte[9];
+            double[] magnitude = new double[width * height]; // Stores the magnitude of the edge response
+            double[] orientation = new double[width * height]; // Stores the angle of the edge at that location
+
+            // First pass - convolve sobel operator and calculate orientation. We're using the byte array now, since it's easier.
+            for (int y = 1; y < height - 1; y++)
+            {
+                for (int x = 1; x < width - 1; x++)
+                {
+                    // Unlike the other Kernel operations, where the radius etc. might change this one is simple so we can hard code
+                    // the kernel operations in. Pointer arithmetic would make this slightly faster, but we won't worry about it.
+                    int index = y * width + x;
+
+                    // 3x3 window around (x,y)
+                    buffer[0] = grayData[index - width - 1];
+                    buffer[1] = grayData[index - width];
+                    buffer[2] = grayData[index - width + 1];
+                    buffer[3] = grayData[index - 1];
+                    buffer[4] = grayData[index];
+                    buffer[5] = grayData[index + 1];
+                    buffer[6] = grayData[index + width - 1];
+                    buffer[7] = grayData[index + width];
+                    buffer[8] = grayData[index + width + 1];
+
+                    // Sobel horizontal and vertical response
+                    double dx = buffer[2] + 2 * buffer[5] + buffer[8] - buffer[0] - 2 * buffer[3] - buffer[6];
+                    double dy = buffer[6] + 2 * buffer[7] + buffer[8] - buffer[0] - 2 * buffer[1] - buffer[2];
+
+                    magnitude[index] = Math.Sqrt(dx * dx + dy * dy) / 1141; // 1141 is approximately the max sobel response, we will normalise later anyway
+
+                    // Directional orientation
+                    orientation[index] = Math.Atan2(dy, dx) + Math.PI; // Angle is in radians, now from 0 - 2PI. 
+                }
+            }
+
+            /* Now that we have the magnitude and orientation, we want to combine these into a coloured image for output.
+             * The HSV colour model would work well here, hue is the angle of the colour, saturation we keep constant at 1,
+             * and value is the magnitude. This should produce an image that is coloured based on edge angle, and whose brightness
+             * reflects edge intensity. */
+
+            // System.Bitmap includes SetPixel and GetPixel methods. These are very slow, so it's better to lock
+            // the image and access the data directly. Locking prevents C# memory management moving it around.
+            BitmapData imageData = image.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, image.PixelFormat);
+
+            uint* ptr = (uint*)imageData.Scan0.ToPointer(); // An unsigned int pointer. This points to the image data in memory, each uint is one pixel ARGB
+            int stride = imageData.Stride / 4; // Stride is the width of one pixel row, including any padding. In bytes, /4 converts to 4 byte pixels 
+
+            byte r, g, b;
+
+            // We want to scale magnitude from 0 - 1, because it's unlikely any magnitude would reach the theoretical maximum value
+            // C#, like other high level languages, contains various list extension methods for ease of use.
+            double magnitudeMax = magnitude.Max();
+
+            // Combine 
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int index = y * width + x;
+
+                    // C# functions can make use of an out parameter. This works much like C/C++ address pointer (&), except it is compiler enforced
+                    // Here we can use it to essentially return three values
+                    double hue = orientation[index];
+                    double val = magnitude[index] / magnitudeMax; // This will still highlight mostly very bright edges, to highlight more try Math.Sqrt(magnitude[index] / magnitudeMax)
+
+                    HSVtoRGB(hue, 1, val, out r, out g, out b); // Using a saturation of 0 will make the image grayscale i.e. regular sobel.
+
+                    // Combine rgb back into the output image
+                    *(ptr + y * stride + x) = (0xFF000000 | (uint)(r << 16) | (uint)(g << 8) | b);
+                }
+            }
+
+            // Finish with image and save
+            image.UnlockBits(imageData);
+
+            return image;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = true;
+            radioButton3.Checked = false;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = true;
+        }
+
+        unsafe public static byte[] ConvertTo8bpp(Bitmap image)
+        {
+            int width = image.Width;
+            int height = image.Height;
+
+            byte[] grayData = new byte[width * height];
+
+            BitmapData imageData = image.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, image.PixelFormat);
+
+            uint* ptr = (uint*)imageData.Scan0.ToPointer();
+            int inputStride = imageData.Stride / 4;
+
+            byte r, g, b;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    // Obtain current pixel data
+                    uint pixel = *(ptr + y * inputStride + x); // Standard formula from the video, px = y * stride + x
+
+                    // Split channels - this uses bit shifting and bitwise AND
+                    r = (byte)((0x00FF0000 & pixel) >> 16);
+                    g = (byte)((0x0000FF00 & pixel) >> 8);
+                    b = (byte)((0x000000FF & pixel));
+
+                    byte gray = (byte)(0.2126 * r + 0.7152 * g + 0.0722 * b);
+                    grayData[y * width + x] = gray;
+                }
+            }
+
+            image.UnlockBits(imageData);
+
+            return grayData;
+        }
+
+        private static void HSVtoRGB(double h, double s, double v, out byte r, out byte g, out byte b)
+        {
+            // h_ 0-6
+            double h_ = h / (2 * Math.PI) * 6;
+
+            double c = s * v;
+            double x = c * (1 - Math.Abs((h_ % 2) - 1));
+            double r_, g_, b_;
+            if (h_ < 1)
+            {
+                r_ = c;
+                g_ = x;
+                b_ = 0;
+            }
+            else if (h_ < 2)
+            {
+                r_ = x;
+                g_ = c;
+                b_ = 0;
+            }
+            else if (h_ < 3)
+            {
+                r_ = 0;
+                g_ = c;
+                b_ = x;
+            }
+            else if (h_ < 4)
+            {
+                r_ = 0;
+                g_ = x;
+                b_ = c;
+            }
+            else if (h_ < 5)
+            {
+                r_ = x;
+                g_ = 0;
+                b_ = c;
+            }
+            else
+            {
+                r_ = c;
+                g_ = 0;
+                b_ = x;
+            }
+
+            double m = v - c;
+
+            r_ += m;
+            g_ += m;
+            b_ += m;
+
+            r = (byte)(r_ * 255);
+            g = (byte)(g_ * 255);
+            b = (byte)(b_ * 255);
         }
     }
 }
